ae938d6d10acff212a571ca1fb637c05cf9f5286
mhumaimidi <mhumaimidi@gmail.com>
2020-06-08 15:53:42 -0700

    new project start point

---
+++ src/myJavaProject/myAppTester.java
@@ -0,0 +1,40 @@
+ /**
+ * Software Engineering - Summer 2020 
+ */
+package myJavaProject;
+
+import java.util.Scanner;
+public class myAppTester {
+	
+ public static void main(String[] args) {
+	  
+	  Scanner input = new Scanner(System.in);
+	  int n=0;
+	  System.out.println("Hello Software Engineering World");
+	  System.out.println("Input your number and press enter: ");
+	  n = input.nextInt();
+	  System.out.print("Reverse of the input number is:");
+	  reverseMethod(n);
+	  System.out.println();
+	  /*
+	   * Git now is on branch b1 
+	   * from now on until switching out of branch b1
+	   * any changes occur here will be tracked in branch b1
+	   * and will not be reflected in any other branch unless b1
+	   * is merged later with that branch 
+	   */
+	}
+	
+	//This is a method to reverse the number by recursion
+	public static void reverseMethod(int num) {
+	       if (num < 10) {
+		   System.out.println(num);
+		   return;
+	       }
+	       else {
+	           System.out.print(num % 10);
+	           reverseMethod(num/10);     // recursive call
+	       }
+	   }
+
+}
